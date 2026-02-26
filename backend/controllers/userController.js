d4d8c8bef87a1e7dbe81efb85f749081ce895a0f
AlansitoFdez <alanfdiosdado@gmail.com>
2026-02-26 16:21:19 +0100

    Login Básico Finalizado

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,21 @@
 const { logMensaje } = require("../utils/logger.js")
+const Respuesta = require("../utils/respuesta.js")
 const userService = require("../services/userService")
 
 class UserController {
-    
+    async login(req, res) {
+        const { email, password } = req.body
+        try {
+            const result = await userService.login(email, password)
+            if (!result) {
+                return res.status(401).json(Respuesta.error(null, "Credenciales inválidas"))
+            }
+            return res.status(200).json(Respuesta.exito(result, "Login exitoso"))
+        } catch (error) {
+            logMensaje(error)
+            return res.status(500).json(Respuesta.error(null, "Error al iniciar sesión"))
+        }
+    }
 }
 
-module.exports = UserController
+module.exports = new UserController()
--- backend/services/userService.js
+++ backend/services/userService.js
@@ -1,11 +1,24 @@
 const initModels = require("../models/init-models").initModels
+const bcrypt = require("bcrypt")
+const Respuesta = require("../utils/respuesta")
 const sequelize = require("../config/sequelize")
 const models = initModels(sequelize)
 
 const User = models.users
 
 class UserService {
-    
+    async login(email, password) {
+        const user = await User.findOne({ where: { email } })
+        if (!user) {
+            throw Respuesta.error(null, "Usuario no encontrado o inexistente")
+        }
+        const validPassword = await bcrypt.compare(password, user.password)
+        if (!validPassword) {
+            throw Respuesta.error(null, "Contraseña incorrecta")
+        }
+
+        return user;
+    }
 }
 
-module.exports = UserService
+module.exports = new UserService()
