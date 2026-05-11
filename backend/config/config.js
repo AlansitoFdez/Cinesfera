729c1b92807392eff01bee11711ff997cd0d4ad5
AlansitoFdez <alanfdiosdado@gmail.com>
2026-05-11 16:14:51 +0200

    fix(config): remove credential logging from config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,5 +1,3 @@
-const { logMensaje } = require("../utils/logger")
-
 require("dotenv").config({
     path: `.env`,
 })
@@ -17,16 +15,4 @@ const config = {
 }
 
 module.exports = config
-
-logMensaje("CONFIG PORT:" + config.db.user);
-
-logMensaje("CONFIG DBHOST:" + config.db.host);
-logMensaje("CONFIG DBPORT:" + config.db.port);
-logMensaje("CONFIG DBNAME:" + config.db.name);
-logMensaje("CONFIG DBUSER:" + config.db.user);
-
-logMensaje("NODE_ENV DBHOST:" + process.env.DB_HOST);
-logMensaje("NODE_ENV DBPORT:" + process.env.DB_PORT);
-logMensaje("NODE_ENV DBNAME:" + process.env.DB_NAME);
-logMensaje("NODE_ENV DBUSER:" + process.env.DB_USER);
 
