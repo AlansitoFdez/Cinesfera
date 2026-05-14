e158c72b13d0f3093c96b380182f8014bacbe14f
DESKTOP-PV7IH63\Alan <alanfdiosdado@gmail.com>
2026-05-14 22:05:08 +0200

    refactor(sequelize): migrate database dialect from MySQL to PostgreSQL

--- backend/config/sequelize.js
+++ backend/config/sequelize.js
@@ -1,38 +1,32 @@
-// config/sequelize.js
 const { logMensaje } = require("../utils/logger.js");
 const { Sequelize } = require("sequelize");
-// Importar fichero de configuración con variables de entorno
 const config = require("./config");
 
-// Instanciar sequelize  para conectar a mysql
 const sequelize = new Sequelize(
-  config.db.name, // nombre bd
-  config.db.user, // usuario
-  config.db.password, // password
+  config.db.name,
+  config.db.user,
+  config.db.password,
   {
-    // objeto con opciones de conexion
-    host: config.db.host, // Cambia esto por la dirección del servidor MySQL
-    port: config.db.port, // Cambia esto por el puerto del servidor MySql
-    dialect: "mysql", // Especificar el dialecto de la base de datos
-    // logging: false, // Desactiva el logging de las consultas SQL
+    host: config.db.host,
+    port: config.db.port,
+    dialect: "postgres",  
     logging: (msg) => {
       if (msg.includes("ERROR")) {
-        console.error("Error de Sequelize:", msg);
+        console.error("Error de Sequelize:", msg)
       }
     },
   }
 );
 
-// Probar la conexión
 (async () => {
   try {
     await sequelize.authenticate();
     if (process.env.NODE_ENV !== "test") {
-      logMensaje("Conexión exitosa a la base de datos MySQL");
+      logMensaje("Conexión exitosa a la base de datos PostgreSQL");
     }
   } catch (error) {
     console.error("Error de conexión:", error);
   }
 })();
 
-module.exports = sequelize; // Exportar la instancia de Sequelize para usarla en otros archivos
+module.exports = sequelize;
