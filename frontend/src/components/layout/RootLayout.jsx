e87b9f173ccf3f71edae7487ce8a3d9333d4b02d
DESKTOP-PV7IH63\Alan <alanfdiosdado@gmail.com>
2026-05-16 17:24:03 +0200

    fix(layout): set root background to #060810 to eliminate grey gaps between pages and footer

--- frontend/src/components/layout/RootLayout.jsx
+++ frontend/src/components/layout/RootLayout.jsx
@@ -4,7 +4,7 @@ import Navbar from "./Navbar";
 
 export default function RootLayout() {
   return (
-    <div className="relative">
+    <div className="relative" style={{ background: "#060810" }}>
       <div className="fixed top-0 left-0 right-0 z-50">
         <Navbar />
       </div>
